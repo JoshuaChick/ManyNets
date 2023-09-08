4d911e5d053358e7884ddb5c94bbc8cc02ffe324
Joshua Chick <josh.sendemails@gmail.com>
2023-09-08 11:24:41 +1000

    Added randomness to selecting training examples, is slow but works.

--- ManyNets.py
+++ ManyNets.py
@@ -3,12 +3,29 @@ import torch.nn as nn
 import torch.nn.functional as f
 import torch.optim as optim
 from torchvision import datasets, transforms
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Dataset
 import matplotlib.pyplot as plt
+import random
 import sys
 import os
 
 
+#################### CONSTANTS ####################
+EPOCHS = 10
+# Max 60,000 (e.g. 10000 means 10000 images will be used for training each separate net)
+NUM_TRAINING_IMGS = 500
+BATCH_SIZE = 100
+NUM_IN_PAINT_EXAMPLES = 10
+NUM_PIXELS_IN_IMAGE = 784
+# Starting at 0
+INDEX_FIRST_PIXEL_TO_PREDICT = 392
+TOTAL_NUM_NETS = NUM_PIXELS_IN_IMAGE - INDEX_FIRST_PIXEL_TO_PREDICT
+LR = 0.001
+IMAGE_OUTPUT_PATH = './generated_images/'
+FOLDER_NAME = IMAGE_OUTPUT_PATH.replace("./", "").replace("/", "")
+
+
+#################### CHECKING CUDA ####################
 if torch.cuda.is_available():
     device = torch.device('cuda')
     print('Using cuda...')
@@ -16,22 +33,11 @@ else:
     device = torch.device('cpu')
     print('Using cpu...')
 
-device = torch.device('cpu')
-print('Overriding to cpu...')
+# device = torch.device('cpu')
+# print('Overriding to cpu...')
 
 
-class Net(nn.Module):
-    def __init__(self, in_nodes, out_nodes):
-        super().__init__()
-        self.l1 = nn.Linear(in_nodes, in_nodes, dtype=torch.float32)
-        self.l2 = nn.Linear(in_nodes, out_nodes, dtype=torch.float32)
-
-    def forward(self, x):
-        x = self.l1(x)
-        x = self.l2(x)
-        return f.softmax(x, dim=0)
-
-
+#################### RANGE CONVERSION FUNCTIONS ####################
 # converts number in range of 0 to 1 to number in range of 0 to 255
 def convert_0_1_to_0_255(x):
     return int(x * 255)
@@ -42,46 +48,129 @@ def convert_0_255_to_0_1(x):
     return x / 255
 
 
-#################### SETTING UP ####################
-EPOCHS = 10
-NUM_IN_PAINT_EXAMPLES = 10
-NUM_PIXELS_IN_IMAGE = 784
-# Starting at 0
-INDEX_FIRST_PIXEL_TO_PREDICT = 392
-TOTAL_NUM_NETS = NUM_PIXELS_IN_IMAGE - INDEX_FIRST_PIXEL_TO_PREDICT
-LR = 0.001
-IMAGE_OUTPUT_PATH = './generated_images/'
-FOLDER_NAME = IMAGE_OUTPUT_PATH.replace("./", "").replace("/", "")
+#################### DATASETS ####################
+class CroppedMnistDatasetTrain(Dataset):
+    """
+    CroppedMnistDatasetTrain will make an object of cropped mnist digits and the next pixel,
+    e.g. the first item may be the first 400 pixels of an image and the 401st pixel as the answer.
+    """
+    def __init__(self, pixels):
+        mnist_train = datasets.MNIST(root='./', train=True, transform=transforms.ToTensor(), download=True)
 
+        mnist_train_cropped_imgs = torch.zeros([len(mnist_train), pixels], dtype=torch.float32).to(device)
+        mnist_train_next_pixel_one_hots = torch.zeros([len(mnist_train), 256], dtype=torch.float32).to(device)
+
+        self.mnist_train_cropped_imgs_capped = torch.zeros([NUM_TRAINING_IMGS, pixels], dtype=torch.float32).to(device)
+        self.mnist_train_next_pixel_one_hots_capped = torch.zeros([NUM_TRAINING_IMGS, 256], dtype=torch.float32).to(device)
+
+        for idx, (image, label) in enumerate(mnist_train):
+            mnist_train_cropped_imgs[idx] = image.view(784)[:pixels]
+
+            next_pixel = image.view(784)[pixels].to(device)
+            next_pixel_0_255 = convert_0_1_to_0_255(float(next_pixel))
+            mnist_train_next_pixel_one_hots[idx] = f.one_hot(torch.tensor(next_pixel_0_255), 256).float().to(device)
+
+        # Selects NUM_TRAINING_IMGS of random examples
+        possible_indexes = [k for k in range(len(mnist_train_cropped_imgs))]
+
+        for counter in range(NUM_TRAINING_IMGS):
+            # Picks an index, then removes it, so it cannot be chosen again
+            random_index = possible_indexes[random.randint(0, len(possible_indexes) - 1)]
+            possible_indexes.remove(random_index)
+
+            self.mnist_train_cropped_imgs_capped[counter] = mnist_train_cropped_imgs[random_index]
+            self.mnist_train_next_pixel_one_hots_capped[counter] = mnist_train_next_pixel_one_hots[random_index]
+
+    def __len__(self):
+        return len(self.mnist_train_cropped_imgs_capped)
+
+    def __getitem__(self, item):
+        return self.mnist_train_cropped_imgs_capped[item], self.mnist_train_next_pixel_one_hots_capped[item]
+
+
+class CroppedMnistDatasetTest(Dataset):
+    """
+    CroppedMnistDatasetTest will make an object of cropped mnist digits, e.g. a dataset of the first 400 pixels of the
+    mnist digits in the test set
+    """
+    def __init__(self, pixels):
+        mnist_test = datasets.MNIST(root='./', train=False, transform=transforms.ToTensor(), download=True)
+
+        self.mnist_test_cropped_imgs = torch.zeros([len(mnist_test), pixels], dtype=torch.float32).to(device)
+
+        for idx, (image, label) in enumerate(mnist_test):
+            self.mnist_test_cropped_imgs[idx] = image.view(784)[:pixels].to(device)
+
+    def __len__(self):
+        return len(self.mnist_test_cropped_imgs)
+
+    def __getitem__(self, item):
+        return self.mnist_test_cropped_imgs[item]
+
+
+#################### NET ####################
+class Net(nn.Module):
+    def __init__(self, in_nodes, out_nodes):
+        super().__init__()
+        self.l1 = nn.Linear(in_nodes, in_nodes, dtype=torch.float32)
+        self.l2 = nn.Linear(in_nodes, out_nodes, dtype=torch.float32)
+
+    def forward(self, x):
+        x = self.l1(x)
+        x = self.l2(x)
+        return f.softmax(x, dim=1)
+
+
+#################### WRITE PERMISSION ####################
 user_agrees_to_output_path = input(f"Type 'y' if data in '{IMAGE_OUTPUT_PATH}' can be overwritten: ")
 
 if user_agrees_to_output_path != 'y':
     sys.exit()
 else:
-    if os.path.exists(IMAGE_OUTPUT_PATH):
-        os.system(f'rd /s /q {FOLDER_NAME}')
-        os.system(f'md {FOLDER_NAME}')
+    if os.name == 'nt':
+        if os.path.exists(IMAGE_OUTPUT_PATH):
+            os.system(f'rd /s /q {FOLDER_NAME}')
+            os.system(f'md {FOLDER_NAME}')
+        else:
+            os.system(f'md {FOLDER_NAME}')
+    elif os.name == 'posix':
+        if os.path.exists(IMAGE_OUTPUT_PATH):
+            os.system(f'rm -r {FOLDER_NAME}')
+            os.system(f'mkdir {FOLDER_NAME}')
+        else:
+            os.system(f'mkdir {FOLDER_NAME}')
     else:
-        os.system(f'md {FOLDER_NAME}')
+        print('OS not recognized, exiting...')
+        sys.exit()
 
-mnist_train = datasets.MNIST(root='./', train=True, transform=transforms.ToTensor(), download=True)
-mnist_test = datasets.MNIST(root='./', train=False, transform=transforms.ToTensor(), download=True)
 
-mnist_train_imgs = torch.zeros([len(mnist_train), 784], dtype=torch.float32).to(device)
-mnist_test_imgs = torch.zeros([len(mnist_test), 784], dtype=torch.float32).to(device)
+#################### INSTANTIATE TRAIN DATASETS AND DATALOADERS ####################
+# NOTE: due to memory constraints train dataset and loader is created during training
 
-num_train_imgs = len(mnist_train_imgs)
-num_test_imgs = len(mnist_test_imgs)
+# train_loaders = []
+#
+# # There will be one loader per net, so 'for i in range(TOTAL_NUM_NETS):' is fine
+# for i in range(TOTAL_NUM_NETS):
+#     train_dataset = CroppedMnistDatasetTrain(INDEX_FIRST_PIXEL_TO_PREDICT + i)
+#     num_batches = len(train_dataset) / BATCH_SIZE
+#     train_loaders.append(DataLoader(train_dataset, shuffle=True, batch_size=BATCH_SIZE))
+#
+#     print(f'{(i + 1) / TOTAL_NUM_NETS * 100:.2F}% loading training data...', end='\r')
+#
+# print('')
+
+
+#################### INSTANTIATE TEST DATASET AND DATALOADER ####################
+test_dataset = CroppedMnistDatasetTest(INDEX_FIRST_PIXEL_TO_PREDICT)
+test_loader = DataLoader(test_dataset, shuffle=True, batch_size=1)
+
+num_test_imgs = len(test_dataset)
 
 assert(NUM_IN_PAINT_EXAMPLES <= num_test_imgs, 'Number of requested in paint examples is higher than test dataset '
                                                'examples, please lower NUM_IN_PAINT_EXAMPLES')
 
-for i, (X, y) in enumerate(mnist_train):
-    mnist_train_imgs[i] = X.view(-1, 784)
 
-for i, (X, y) in enumerate(mnist_test):
-    mnist_test_imgs[i] = X.view(-1, 784)
-
+#################### INSTANTIATE NETS AND OPTIMIZERS ####################
 nets = []
 optimizers = []
 
@@ -103,45 +192,38 @@ for i in range(TOTAL_NUM_NETS):
 
 #################### TRAINING AND TESTING ####################
 for epoch in range(EPOCHS):
-    for m, img in enumerate(mnist_train_imgs):
-        for i in range(TOTAL_NUM_NETS):
+    for i in range(TOTAL_NUM_NETS):
+        train_dataset = CroppedMnistDatasetTrain(INDEX_FIRST_PIXEL_TO_PREDICT + i)
+        num_batches = len(train_dataset) / BATCH_SIZE
+        train_loader = DataLoader(train_dataset, shuffle=True, batch_size=BATCH_SIZE)
+
+        for X, y in train_loader:
             optimizers[i].zero_grad()
 
             num_of_pixels = input_pixels_for_each_net[i]
-            cropped_img = img[:num_of_pixels]
 
-            pixel_to_predict = img[num_of_pixels]
-            pixel_to_predict_0_255 = convert_0_1_to_0_255(float(pixel_to_predict))
+            output = nets[i](X)
 
-            output = nets[i](cropped_img)
-
-            one_hot = f.one_hot(torch.tensor(pixel_to_predict_0_255), 256).float().to(device)
-
-            loss = f.mse_loss(output, one_hot)
+            loss = f.mse_loss(output, y)
             loss.backward()
 
             optimizers[i].step()
 
-        print(f'Epoch {epoch + 1} {(m + 1) / num_train_imgs * 100:.2F}% completed. Loss = {loss}.', end='\r')
+        print(f'Epoch {epoch + 1} {(i + 1) / TOTAL_NUM_NETS * 100:.2F}% completed. Loss = {loss}.', end='\r')
 
     print('')
 
     with torch.no_grad():
-        for j, img in enumerate(mnist_test_imgs):
+        for j, img in enumerate(test_loader):
             if j == NUM_IN_PAINT_EXAMPLES:
                 break
 
-            starting_num_of_pixels = input_pixels_for_each_net[0]
-            in_paint_example = img[:starting_num_of_pixels]
-
             for i in range(TOTAL_NUM_NETS):
-                predicted_pixel_brightness = convert_0_255_to_0_1(int(torch.argmax(nets[i](in_paint_example))))
+                predicted_pixel_brightness = convert_0_255_to_0_1(int(torch.argmax(nets[i](img)[0])))
                 predicted_pixel_brightness = torch.tensor([predicted_pixel_brightness], dtype=torch.float32).to(device)
-                in_paint_example = torch.cat((in_paint_example, predicted_pixel_brightness))
+                img = torch.cat((img[0], predicted_pixel_brightness)).view(1, INDEX_FIRST_PIXEL_TO_PREDICT + i + 1)
 
-            plt.imsave(f'{IMAGE_OUTPUT_PATH}Epoch{epoch + 1}Example{j + 1}.png', in_paint_example.view(28, 28))
-
-
+            plt.imsave(f'{IMAGE_OUTPUT_PATH}Epoch{epoch + 1}Example{j + 1}.png', img.to(torch.device('cpu')).view(28, 28))
 
 
 
